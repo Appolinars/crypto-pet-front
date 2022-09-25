d1b2d4e0fe1265401653f252187f67f256c471e3
appolinaris <vakulenko.maksim977@gmail.com>
2022-09-26 00:13:52 +0300

    dynamic toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import NextNProgress from 'nextjs-progressbar';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, ToastContainerProps } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { TypeComponentAuthFields } from 'src/shared/types/authTypes';
 
 import MainProvider from '@/providers/MainProvider';
 
 import { colorAccent } from '@/configs/constants';
 
-import { TypeComponentAuthFields } from 'src/shared/types/authTypes';
-
 import '@/styles/app.scss';
+
+const DynamicToastContainer = dynamic<ToastContainerProps>(() =>
+  import('react-toastify').then((mod) => mod.ToastContainer)
+);
 
 type TypeAppProps = AppProps & TypeComponentAuthFields;
 
@@ -19,7 +23,7 @@ function MyApp({ Component, pageProps }: TypeAppProps) {
       <MainProvider Component={Component}>
         <Component {...pageProps} />
       </MainProvider>
-      <ToastContainer theme="dark" />
+      <DynamicToastContainer theme="dark" />
       <NextNProgress color={colorAccent} options={{ showSpinner: false }} />
     </>
   );
